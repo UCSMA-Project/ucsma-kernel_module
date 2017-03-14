062ff55f1058b8875fe003853093b2b74be61b9b
JackWindows <fhb1992@gmail.com>
2017-03-14 17:50:04 -0400

    switch to getnstimeofday(), better performance and precision

--- interrupt_test_node1.c
+++ interrupt_test_node1.c
@@ -16,8 +16,7 @@ struct gpio unlock_gpios[] = {
 static int __init unlock_init(void)
 {
   int ret, i;
-  unsigned long int sec, usec;
-  struct timeval start, end;
+  struct timespec start, end, delta;
 
   printk(KERN_INFO "[Interrupt test] Init\n");
 
@@ -30,16 +29,17 @@ static int __init unlock_init(void)
   gpio_set_value(unlock_gpios[0].gpio, 1);
   gpio_set_value(unlock_gpios[0].gpio, 0);
   
-  do_gettimeofday(&start);
+  getnstimeofday(&start);
   for (i = 0; i < 1000000; i++) {
     if (gpio_get_value(unlock_gpios[1].gpio))
       break;
   }
-  do_gettimeofday(&end);
-  sec = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
-  usec = sec % 1000000;
-  sec /= 1000000;
-  printk(KERN_INFO "[Interrupt test] GPIO polling %d times in %lu.%06lus\n", i, sec, usec);
+  getnstimeofday(&end);
+  delta = timespec_sub(end, start);
+  if (delta.tv_sec > 0)
+    printk(KERN_ERR "[Interrupt test] GPIO delay > 1s, this should never happen\n");
+  else
+    printk(KERN_INFO "[Interrupt test] GPIO polling %d times in %lu ns\n", i, delta.tv_nsec);
 
   return 0;
 
