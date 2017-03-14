4ce1b63e03d0133eff9f4fade5e9bc612e19f732
JackWindows <fhb1992@gmail.com>
2017-03-14 18:48:12 -0400

    reform interrupt_test_node1, use timer to do multiple tests each time

--- interrupt_test_node1.c
+++ interrupt_test_node1.c
@@ -6,17 +6,77 @@
 #include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
+#include <linux/moduleparam.h>
+
+
+#define TEST_INTERVAL	2
 
 struct gpio unlock_gpios[] = {
   {21, GPIOF_OUT_INIT_LOW, "UNLOCK_OUT"},
   {22, GPIOF_IN, "UNLOCK_IN" },
 };
 
+struct irq_latency_test {
+  struct timespec start, end;
+  struct timer_list timer;
+  u32 test_count, missed_count, max_count;
+  unsigned long avg_nsecs, min_nsecs, max_nsecs;
+};
+
+static u32 test_count = 100;
+module_param(test_count, uint, 0);
+
+static struct irq_latency_test test_data;
+
+static void interrupt_test_node1_timer_handler(unsigned long ptr) {
+  struct irq_latency_test* data = (struct irq_latency_test*)ptr;
+  int i = 0, test_ok = 0;
+
+  gpio_set_value(unlock_gpios[0].gpio, 1);
+  getnstimeofday(&data->start);
+  gpio_set_value(unlock_gpios[0].gpio, 0);
+
+  for (i = 0; i < 1000000; i++) {
+    if (gpio_get_value(unlock_gpios[1].gpio))
+      break;
+  }
+
+  getnstimeofday(&data->end);
+
+  if (i < 1000000) {
+    struct timespec delta = timespec_sub(data->end, data->start);
+    if (delta.tv_sec > 0) {
+      printk(KERN_ERR "[Interrupt test] GPIO IRQ > 1 sec, this should never happen\n");
+      data->missed_count++;
+    }
+    else {
+      data->avg_nsecs = (data->avg_nsecs * data->test_count + delta.tv_nsec) / (data->test_count + 1);
+      data->max_nsecs = (delta.tv_nsec > data->max_nsecs) ? delta.tv_nsec : data->max_nsecs;
+      data->min_nsecs = (delta.tv_nsec < data->min_nsecs || !data->min_nsecs) ? delta.tv_nsec : data->min_nsecs;
+      test_ok = 1;
+    }
+  }
+  else {
+    data->missed_count++;
+  }
+
+  if (test_ok && ++data->test_count >= data->max_count) {
+    printk(KERN_INFO "[Interrupt test] finished %u passes. average GPIO IRQ latency is %lu nsecs, "
+                      "minimum latency is %lu nsecs, maximum latency is %lu nsecs\n",
+          data->max_count, data->avg_nsecs, data->min_nsecs, data->max_nsecs);
+    return;
+  }
+
+  mod_timer(&data->timer, jiffies + TEST_INTERVAL);
+
+  return;
+
+}
+
 /* Entry point of driver */
 static int __init unlock_init(void)
 {
-  int ret, i;
-  struct timespec start, end, delta;
+  int ret;
 
   printk(KERN_INFO "[Interrupt test] Init\n");
 
@@ -26,20 +86,15 @@ static int __init unlock_init(void)
 	goto fail;
   }
 
-  gpio_set_value(unlock_gpios[0].gpio, 1);
-  gpio_set_value(unlock_gpios[0].gpio, 0);
-  
-  getnstimeofday(&start);
-  for (i = 0; i < 1000000; i++) {
-    if (gpio_get_value(unlock_gpios[1].gpio))
-      break;
-  }
-  getnstimeofday(&end);
-  delta = timespec_sub(end, start);
-  if (delta.tv_sec > 0)
-    printk(KERN_ERR "[Interrupt test] GPIO delay > 1s, this should never happen\n");
-  else
-    printk(KERN_INFO "[Interrupt test] GPIO polling %d times in %lu ns\n", i, delta.tv_nsec);
+  init_timer(&test_data.timer);
+  test_data.timer.expires = jiffies + TEST_INTERVAL;
+  test_data.timer.data = (unsigned long)&test_data;
+  test_data.timer.function = interrupt_test_node1_timer_handler;
+  add_timer(&test_data.timer);
+
+  test_data.max_count = test_count;
+  printk(KERN_INFO "[Interrupt test] beginning GPIO IRQ latency test %u passes\n",
+    test_data.max_count);
 
   return 0;
 
@@ -52,6 +107,7 @@ fail:
 /* Exit Point of driver */
 static void __exit unlock_exit(void)
 {
+  del_timer_sync(&test_data.timer);
   gpio_free_array(unlock_gpios, ARRAY_SIZE(unlock_gpios));
   printk(KERN_INFO "[Interrupt test] Uninit\n");
   return;
